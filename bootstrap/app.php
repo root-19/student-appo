5b05445d998cef2ee6fcd7069dadd30b65547db9
Rens  Acuña <127023454+root-19@users.noreply.github.com>
2026-03-15 00:00:20 -0700

    push

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Illuminate\Http\Middleware\HandleCors::class,
         ]);
+        
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
